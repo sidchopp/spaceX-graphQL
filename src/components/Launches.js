ee2cc4fcc2d6c5105281087b7cd358229a5250a1
sidchopp <sidchopp@gmail.com>
2022-07-10 14:10:27 -0600

    Past launches are shown as cards

--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,6 +1,14 @@
 import { useGlobalContext } from "./Context";
 import { useQuery } from "@apollo/client";
 import { GET_LAUNCHES } from "../queries/queries";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 //Components
 import Loader from "./Loader";
@@ -10,7 +18,7 @@ const Launches = () => {
   console.log(data);
   if (error) return <p>`Error :( ${error.message}`</p>;
 
-  //To show last 10 launches
+  //To show last  launches
   const showPastLaunches = () => {
     // if data is not fetched,show loader
     if (loading) {
@@ -21,10 +29,55 @@ const Launches = () => {
       });
     }
   };
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <div>
       <h1>Space X Launches</h1>
-      <ul>{showPastLaunches()}</ul>
+      {/* <ul>{showPastLaunches()}</ul> */}
+      <Container sx={{ py: 8 }} maxWidth="xl">
+        <Grid container spacing={4}>
+          {data.launchesPast.map((launch) => (
+            <Grid item key={launch.id} xs={12} sm={6} md={3}>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                {/* <CardMedia
+                  component="img"
+                  sx={{
+                    // 16:9
+                    pt: "56.25%",
+                  }}
+                  image="https://source.unsplash.com/random"
+                  alt="random"
+                /> */}
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {launch.mission_name}
+                  </Typography>
+                  <Typography>
+                    Launched from: {launch.launch_site.site_name_long}
+                  </Typography>
+                  <Typography>Launched in: {launch.launch_year}</Typography>
+                  <Typography>
+                    Launched at: {launch.launch_date_local}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">View</Button>
+                  <Button size="small">Edit</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 };
