605e33e93c5648ca0631671b33f8020b4c0f1b75
sidchopp <sidchopp@gmail.com>
2022-07-10 11:46:54 -0600

    key prop is set in list of last 10 launches

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Loader from "./components/Loader";
 
 function App() {
   const { loading, launches } = useGlobalContext();
-  console.log(launches);
+  // console.log(launches);
   // if data is not fetched,show loader
   if (loading) {
     return <Loader />;
@@ -14,7 +14,7 @@ function App() {
       <h1>Space X Launches</h1>
       <ul>
         {launches.map((launch) => {
-          return <li>{launch.mission_name}</li>;
+          return <li key={launch.id}>{launch.mission_name}</li>;
         })}
       </ul>
     </div>
