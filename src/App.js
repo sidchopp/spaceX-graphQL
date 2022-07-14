a0075f71b76ad413091350424741c513cef6a671
sidchopp <sidchopp@gmail.com>
2022-07-14 11:01:33 -0600

    dark theme removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useGlobalContext } from "./components/Context";
 import { ApolloProvider } from "@apollo/client";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Components
@@ -8,25 +7,17 @@ import Launches from "./components/Launches";
 import Rockets from "./components/Rockets";
 import AboutCompany from "./components/AboutCompany";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
-
 function App() {
   const { client } = useGlobalContext();
   return (
     <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <ApolloProvider client={client}>
-          <Routes>
-            <Route path="/" element={<AboutCompany />}></Route>
-            <Route path="/rockets" element={<Rockets />}></Route>
-            <Route path="/launches" element={<Launches />}></Route>
-          </Routes>
-        </ApolloProvider>
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<AboutCompany />}></Route>
+          <Route path="/rockets" element={<Rockets />}></Route>
+          <Route path="/launches" element={<Launches />}></Route>
+        </Routes>
+      </ApolloProvider>
     </BrowserRouter>
   );
 }
